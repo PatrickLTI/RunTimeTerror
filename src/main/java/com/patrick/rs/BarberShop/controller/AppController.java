07430f08f82848219e3e44f1a2e343791deb9706
Pat <la_spirou@hotmail.com>
2022-11-03 14:03:52 -0400

    adding changes in patrick branch, haven't pushed to master yet

--- src/main/java/com/patrick/rs/BarberShop/controller/AppController.java
+++ src/main/java/com/patrick/rs/BarberShop/controller/AppController.java
@@ -1,14 +1,64 @@
 package com.patrick.rs.BarberShop.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;*/
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.patrick.rs.BarberShop.model.User;
+import com.patrick.rs.BarberShop.service.RegistrationServices;
 
 @Controller
 public class AppController {
 
-	@GetMapping("/")
-	public String showHeader() {
-		return "index";	
+	@Autowired
+	private RegistrationServices registrationService;
+
+	@RequestMapping("/registration")
+	public String showRegistration(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "registration";
+	}
+
+	@PostMapping(value = "/registerUser")
+	public String registerUsers(@Valid User user, Errors errors, Model model, BindingResult bindingResult) {
+
+		if (user.getPassword() != null && user.getSimplePassword() != null) {
+			if (!user.getPassword().equals(user.getSimplePassword())) {
+
+				bindingResult.addError(new FieldError("user", "simplePassword", "Passwords must match."));
+				return "registration";
+			}
+		}
+
+		if (null != errors && errors.getErrorCount() > 0) {
+			return "registration";
+
+		} else {
+			/*
+			 * BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); String
+			 * encodedPassword = passwordEncoder.encode(user.getPassword());
+			 * user.setPassword(encodedPassword);
+			 * 
+			 * user.setSimplePassword(encodedPassword);
+			 */
+			registrationService.save(user);
+			return "new_users";
+//			userdashboard
+		}
 	}
 
 }
---
+++ src/main/java/com/patrick/rs/BarberShop/model/User.java
@@ -0,0 +1,115 @@
+package com.patrick.rs.BarberShop.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotBlank
+	@Email(message = "Please enter a valid email address")
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	@Size(min = 5, max = 20)
+	@Column(name = "fullName", nullable = false)
+	private String fullName;
+
+	@NotBlank
+	@Column(name = "phoneNumber", nullable = false)
+	private String phoneNumber;
+
+	@NotBlank(message = "Password should not be blank")
+	@Size(min = 5, max = 20, message = "Password must be between 5 - 20 characters")
+	@Column(name = "confirmPassword", nullable = false)
+	private String simplePassword;
+
+	@NotBlank(message = "Password should not be blank")
+	@Size(min = 5, max = 20, message = "Password must be between 5 - 20 characters")
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	private boolean isAdmin;
+
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	private Date createdAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getSimplePassword() {
+		return simplePassword;
+	}
+
+	public void setSimplePassword(String simplePassword) {
+		this.simplePassword = simplePassword;
+	}
+
+}
