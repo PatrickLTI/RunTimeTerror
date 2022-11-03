fb2e0ec79a96dbf8160e5cfea7a0540f868775ce
Aftab Safdar <safdar_aftab@hotmail.com>
2022-11-03 03:02:56 -0400

    Header changes + Index page

--- src/main/java/com/patrick/rs/BarberShop/repositories/AppointmentRepo.java
+++ src/main/java/com/patrick/rs/BarberShop/repositories/AppointmentRepo.java
@@ -1,17 +1,17 @@
 package com.patrick.rs.BarberShop.repositories;
 
-import com.patrick.rs.BarberShop.models.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.sql.Date;
-import java.util.List;
-import java.util.Optional;
-
-public interface AppointmentRepo extends JpaRepository<Appointment, Long> {
-    public Optional<List<Appointment>> findAllByUserId(long userId);
-
-    public Optional<List<Appointment>> findAllByAppDate(Date date);
-
-    public void deleteAllByUserId(long userId);
-
-}
+//import com.patrick.rs.BarberShop.models.Appointment;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.sql.Date;
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface AppointmentRepo extends JpaRepository<Appointment, Long> {
+//    public Optional<List<Appointment>> findAllByUserId(long userId);
+//
+//    public Optional<List<Appointment>> findAllByAppDate(Date date);
+//
+//    public void deleteAllByUserId(long userId);
+//
+//}
