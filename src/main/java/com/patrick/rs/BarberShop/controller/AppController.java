40d24b60c70672929e2b24b08b254d5dd20ce751
Aftab Safdar <safdar_aftab@hotmail.com>
2022-11-04 16:29:19 -0400

    Created AdminDashboard - Implementing Calendar next

--- src/main/java/com/patrick/rs/BarberShop/controller/AppController.java
+++ src/main/java/com/patrick/rs/BarberShop/controller/AppController.java
@@ -1,9 +1,9 @@
 package com.patrick.rs.BarberShop.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
+import com.patrick.rs.BarberShop.model.Appointment;
+import com.patrick.rs.BarberShop.model.User;
+import com.patrick.rs.BarberShop.services.AppointmentService;
+import com.patrick.rs.BarberShop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -11,137 +11,144 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.patrick.rs.BarberShop.model.Appointment;
-import com.patrick.rs.BarberShop.model.User;
-import com.patrick.rs.BarberShop.services.AppointmentService;
-import com.patrick.rs.BarberShop.services.UserService;
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class AppController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private AppointmentService appointmentService;
+    @Autowired
+    private AppointmentService appointmentService;
 
-	@RequestMapping("/")
-	public String showIndex() {
-		return "index";
-	}
+    @RequestMapping("/")
+    public String showIndex() {
+        return "index";
+    }
 
-	@GetMapping("/login")
-	public String showLogin(Model model) {
-		User user = new User();
-		model.addAttribute("user", user);
-		
-		return "login";
-	}
-	
-	@GetMapping("logout")
+    @GetMapping("/login")
+    public String showLogin(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+
+        return "login";
+    }
+
+    @GetMapping("logout")
     public String logout() {
         SecurityContextHolder.getContext().setAuthentication(null);
         return "redirect:/";
     }
 
-	@RequestMapping("/registration")
-	public String showRegistration(Model model) {
-		User user = new User();
-		model.addAttribute("user", user);
-		return "registration";
-	}
+    @RequestMapping("/registration")
+    public String showRegistration(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "registration";
+    }
 
-	@GetMapping("/pricing")
-	public String showPricePage() {
-		return "pricePage";
-	}
+    @GetMapping("/pricing")
+    public String showPricePage() {
+        return "pricePage";
+    }
 
-	@PostMapping(value = "/registerUser")
-	public String registerUsers(@Valid User user, Errors errors, Model model, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    @PostMapping(value = "/registerUser")
+    public String registerUsers(@Valid User user, Errors errors, Model model, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
-		if (user.getPassword() != null && user.getSimplePassword() != null) {
-			if (!user.getPassword().equals(user.getSimplePassword())) {
+        if (user.getPassword() != null && user.getSimplePassword() != null) {
+            if (!user.getPassword().equals(user.getSimplePassword())) {
 
-				bindingResult.addError(new FieldError("user", "simplePassword", "Passwords must match."));
-				return "registration";
-			}
-		}
+                bindingResult.addError(new FieldError("user", "simplePassword", "Passwords must match."));
+                return "registration";
+            }
+        }
 
-		if (null != errors && errors.getErrorCount() > 0) {
-			return "registration";
+        if (null != errors && errors.getErrorCount() > 0) {
+            return "registration";
 
-		}
+        } else {
 
-		else {
-			
-			
-			try {
-				userService.create(user);
-			} catch (Exception e) {
-				if (e.getMessage() == UserService.EMAIL_TAKEN) {
-					bindingResult.addError(new FieldError("user", "email", "Email taken."));
-				}
-				if (e.getMessage() == UserService.PHONENUMBER_TAKEN) {
-					bindingResult.addError(new FieldError("user", "phoneNumber", "Phone number taken."));
-				}
-				return "registration";
-			}
-			redirectAttributes.addAttribute("newUser", "true");
+
+            try {
+                userService.create(user);
+            } catch (Exception e) {
+                if (e.getMessage() == UserService.EMAIL_TAKEN) {
+                    bindingResult.addError(new FieldError("user", "email", "Email taken."));
+                }
+                if (e.getMessage() == UserService.PHONENUMBER_TAKEN) {
+                    bindingResult.addError(new FieldError("user", "phoneNumber", "Phone number taken."));
+                }
+                return "registration";
+            }
+            redirectAttributes.addAttribute("newUser", "true");
             return "redirect:/login";
-			
 
-		}
-	}
 
-	@GetMapping("/userdashboard")
-	public String showDashboard(Model model) {
-		List<Appointment> listAppoints = appointmentService.getAll();
-		model.addAttribute("listAppoints", listAppoints);
+        }
+    }
 
-		List<Appointment> listPastAppoints = appointmentService.getAll();
-		model.addAttribute("listPastAppoints", listPastAppoints);
+    @GetMapping("/admindashboard")
+    public String showAdminDashboard(Model model) {
+        Map<Boolean, List<Appointment>> appointments = appointmentService
+                .getAll()
+                .stream()
+                .collect(Collectors.partitioningBy(appointment -> appointment.getAppDate().before(new Date(System.currentTimeMillis()))));
 
-		return "userdashboard";
+        model.addAttribute("pastAppointments", appointments.get(true));
+        model.addAttribute("futureAppointments", appointments.get(false));
+        return "admindashboard";
+    }
 
-	}
+    @GetMapping("/userdashboard")
+    public String showDashboard(Model model) {
+        List<Appointment> listAppoints = appointmentService.getAll();
+        model.addAttribute("listAppoints", listAppoints);
 
-	@GetMapping("/edit_appoint/{id}")
-	public String showEditAppointmentPage(@PathVariable(name = "id") long id, Model model) {
-		model.addAttribute("appointment", appointmentService.findById(id));
-		return "edit_appoint";
-	}
+        List<Appointment> listPastAppoints = appointmentService.getAll();
+        model.addAttribute("listPastAppoints", listPastAppoints);
 
-	@PostMapping("/edit_appoint/save")
-	public String updateAppointment(@ModelAttribute("appointment") Appointment appointment) {
-		appointmentService.save(appointment);
-		return "userdashboard";
-	}
+        return "userdashboard";
 
-	@GetMapping("/delete_appoint/{id}")
-	public String deleteAppointment(@PathVariable(name = "id") long id) {
-		appointmentService.delete(id);
-		return "redirect:/userdashboard";
-	}
+    }
 
-	@GetMapping("/appointment")
-	public String bookAppointment(Appointment appointment) {
-		return "appointment";
-	}
+    @GetMapping("/edit_appoint/{id}")
+    public String showEditAppointmentPage(@PathVariable(name = "id") long id, Model model) {
+        model.addAttribute("appointment", appointmentService.findById(id));
+        return "edit_appoint";
+    }
 
-	@PostMapping("/appointment")
-	public String submitForm(@Valid Appointment appointment, BindingResult bindingResult, Model model) {
-		if (bindingResult.hasErrors()) {
-			return "appointment";
-		}
+    @PostMapping("/edit_appoint/save")
+    public String updateAppointment(@ModelAttribute("appointment") Appointment appointment) {
+        appointmentService.save(appointment);
+        return "userdashboard";
+    }
 
-		appointmentService.save(appointment);
-		return "appointmentbooked";
-	}
+    @GetMapping("/delete_appoint/{id}")
+    public String deleteAppointment(@PathVariable(name = "id") long id) {
+        appointmentService.delete(id);
+        return "redirect:/userdashboard";
+    }
+
+    @GetMapping("/appointment")
+    public String bookAppointment(Appointment appointment) {
+        return "appointment";
+    }
+
+    @PostMapping("/appointment")
+    public String submitForm(@Valid Appointment appointment, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "appointment";
+        }
+
+        appointmentService.save(appointment);
+        return "appointmentbooked";
+    }
 }
