123640481e23e98eca5b6a2df6a41dcb2d980d0a
Rrc <rrc@s-3.local>
2022-11-05 22:18:39 -0400

    add edit_user controller and html page

--- src/main/java/com/patrick/rs/BarberShop/controller/AppController.java
+++ src/main/java/com/patrick/rs/BarberShop/controller/AppController.java
@@ -162,6 +162,24 @@ public class AppController {
 		return "redirect:/userdashboard";
 	}
 
+	@GetMapping("/edit_user/{id}")
+	public String showEditUserPage(@PathVariable(name = "id") long id, Model model) {;
+//		User user = userService.findById(id);
+//		user.setPassword(user.getPassword());
+//		user.setSimplePassword(user.getSimplePassword());
+		model.addAttribute("user", userService.findById(id));
+		return "edit_user";
+	}
+
+	@PostMapping("/edit_user")
+	public String updateUser(@Valid User user, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "edit_user";
+		}
+		userService.update(user.getId(), user);
+		return "redirect:/userdashboard";
+	}
+
 	@GetMapping("/appointment")
 	public String bookAppointment(Model model) {
 			model.addAttribute("appointment", new Appointment());
