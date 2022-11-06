4cf1bbb036228fe29365c980ddb35d65152f89e9
Pat <la_spirou@hotmail.com>
2022-11-06 15:16:12 -0500

    app controller merge conflict

--- src/main/java/com/patrick/rs/BarberShop/controller/AppController.java
+++ src/main/java/com/patrick/rs/BarberShop/controller/AppController.java
@@ -74,13 +74,6 @@ public class AppController {
 		return "pricePage";
 	}
 
-	/* @EventListener(ApplicationReadyEvent.class) */
-
-	/*
-	 * public void sendMail() throws MessagingException {
-	 * senderService.sendEmail("kutzxpression@gmail.com", "this is Subject",
-	 * "This is body of Email"); }
-	 */
 
 	@PostMapping(value = "/registerUser")
 	public String registerUsers(@Valid User user, Errors errors, Model model, BindingResult bindingResult,
@@ -180,20 +173,13 @@ public class AppController {
 	}
 
 	@GetMapping("/edit_user/{id}")
-<<<<<<< HEAD
-	public String showEditUserPage(@PathVariable(name = "id") long id, Model model) {
-		;
-//		User user = userService.findById(id);
-//		user.setPassword(user.getPassword());
-//		user.setSimplePassword(user.getSimplePassword());
-		model.addAttribute("user", userService.findById(id));
-=======
+
 	public String showEditUserPage(@PathVariable(name = "id") long id, Model model) {;
 		User user = userService.findById(id);
 		user.setPassword("12345");
 		user.setSimplePassword("12345");
 		model.addAttribute("user", user);
->>>>>>> ae7e56eab13208e5455a4c7004e7b7bb93908e4f
+
 		return "edit_user";
 	}
 
